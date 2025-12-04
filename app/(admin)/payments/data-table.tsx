753059d0366591a5bdc5b219e15fcfdf50b20b05
danyoh <danyoh#gmail.com>
2025-12-04 16:50:15 +0900

    1

--- app/(admin)/payments/data-table.tsx
+++ app/(admin)/payments/data-table.tsx
@@ -150,10 +150,7 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
+
         <div className="flex items-center justify-between px-2">
           <div className="flex-1 text-sm text-muted-foreground">
             Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
